643a815d70c23c57f049ef5db368d316964be801
zinebt02 <112364305+zinebt02@users.noreply.github.com>
2024-04-08 20:15:02 -0400

    Update analysisScreen.js

--- analysisScreen.js
+++ analysisScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { FIREBASE_DB } from './App';
+import {FIREBASE_DB as db} from "./firebase-config";
 
 const AnalysisScreen = () => {
   const [data, setData] = useState({
