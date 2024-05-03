415769af83acbddc9391575555f36257a852c881
octtenz <89526286+octtenz@users.noreply.github.com>
2024-05-03 18:26:19 -0400

    Add comments and validate date inputs

--- TaskDetail.js
+++ TaskDetail.js
@@ -1,15 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  Keyboard,
-  ScrollView,
-  StyleSheet,
-  Alert,
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView, StyleSheet, Alert } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
 import { FontAwesome } from '@expo/vector-icons';
 import { FIREBASE_DB } from './firebase-config.js';
@@ -18,7 +8,13 @@ import AddTagsModal from './addTagPopup.js';
 import SetReminderScreen from './setReminderPopup.js';
 import * as Notifications from 'expo-notifications';
 
+/**
+ * Task Detail screen handles the creation and editing of the goals and tasks
+ * @param {*} param0 Object containing navigation and route properties
+ * @returns React element representing the Task Detail screen
+ */
 const TaskDetailScreen = ({ navigation, route }) => {
+  // State variables for task details
   const [title, setTitle] = useState('');
   const [note, setNote] = useState('');
   const [parentTask, setParentTask] = useState('');
@@ -32,20 +28,22 @@ const TaskDetailScreen = ({ navigation, route }) => {
   const [expectedTime, setExpectedTime] = useState('');
   const [unit, setUnit] = useState(''); 
   const [notificationScheduled, setNotificationScheduled] = useState(false);
-
   const tasks = route.params.tasks;
   const [existingTasks, setExistingTasks] = useState([]);
 
+  // Update existing tasks when the tasks prop changes
   useEffect(() => {
     if (tasks) {
       setExistingTasks(tasks.map(task => task.id));
     }
   }, [tasks]);
 
+  // Load data when the tasks prop changes
   useEffect(() => {
     loadData();
   }, [tasks]);
 
+  // Load data for editing the current task
   const loadData = () => {
     if(route.params.currentTaskID != null){
       const currentTask = tasks.find(obj => obj.id === route.params.currentTaskID);
@@ -67,6 +65,10 @@ const TaskDetailScreen = ({ navigation, route }) => {
     }
   }
 
+  /**
+   * Handles the selection of a parent task
+   * @param {*} index The index of the selected task in the existing tasks array
+   */
   const handleParentTask = (index) => {
     const selectedTaskId = existingTasks[index]; 
     const selectedTask = tasks.find(task => task.id === selectedTaskId); 
@@ -74,22 +76,35 @@ const TaskDetailScreen = ({ navigation, route }) => {
     setParentTask(selectedTaskTitle);
   };
 
+  /**
+   * Removes a tag from the existing tags array
+   * @param {*} index The index of the tag to be removed in the existing tags array
+   */
   const removeTag = (index) => {
     const updatedTags = [...selectedTags];
     updatedTags.splice(index, 1);
     setSelectedTags(updatedTags);
   };
 
+  /**
+   * Handles the selection of a tag
+   * @param {*} tag The tag to be selected
+   */
   const handleTagSelect = (tag) => {
     setSelectedTags([...selectedTags, { text: tag, selected: false }]);
   };
 
+  /**
+   * Toggles the selection state of a tag
+   * @param {*} index The index of the tag to toggle in the existing tags array
+   */
   const toggleTagSelection = (index) => {
     const updatedTags = [...selectedTags];
     updatedTags[index].selected = !updatedTags[index].selected;
     setSelectedTags(updatedTags);
   };
 
+  // Cancels the scheduled notification
   const cancelNotification = async () => {
     try {
       await Notifications.cancelAllScheduledNotificationsAsync();
@@ -97,50 +112,53 @@ const TaskDetailScreen = ({ navigation, route }) => {
       console.log('Notification cancelled');
     } catch (error) {
       console.error('Error cancelling notification:', error.message);
-      Alert.alert('Error', 'Failed to cancel notification. Please try again later.');
+      Alert.alert('Error', 'Failed to cancel notification');
     }
   };
 
+  // Handles the cancellation of the task creation or editing process
   const handleCancel = () => {
     console.log('Cancelled');
     cancelNotification();
     navigation.navigate('Home', route.params);
   };
 
+  // Handles the saving of the task creation or editing process
   const handleSave = async () => {
+    // Check if the title field is empty
     if (!title.trim()) {
       Alert.alert('Title field is required');
       return;
     }
 
+    // Validate day and month inputs
+    if (parseInt(startDateDay) < 1 || parseInt(startDateDay) > 31 || 
+        parseInt(dueDateDay) < 1 || parseInt(dueDateDay) > 31 ) {
+      Alert.alert('Invalid day input');
+      return;
+    }
+    if (parseInt(startDateMonth) < 1 || parseInt(startDateMonth) > 12 ||
+        parseInt(dueDateMonth) < 1 || parseInt(dueDateMonth) > 12) {
+      Alert.alert('Invalid month input');
+      return;
+    }
+
+    // Construct start date, due date, and expected time strings
     const startDate =
       startDateDay && startDateMonth && startDateYear
         ? startDateDay + startDateMonth + startDateYear
         : null;
-
     const dueDate =
       dueDateDay && dueDateMonth && dueDateYear
         ? dueDateDay + dueDateMonth + dueDateYear
         : null;
-
     const expectedTimeString = `${expectedTime} ${unit}`;
 
-    console.log('Saved:', {
-      title,
-      note,
-      parentTask,
-      selectedTags,
-      startDateDay,
-      startDateMonth,
-      startDateYear,
-      dueDateDay,
-      dueDateMonth,
-      dueDateYear,
-      expectedTime,
-      unit,
-    });
+    console.log('Saved:', {title, note, parentTask, selectedTags, startDateDay, startDateMonth, startDateYear, dueDateDay, dueDateMonth, dueDateYear, expectedTime, unit});
 
+    // If a new task is being created
     if (route.params.currentTaskID == null){
+      // Add a new task document to the Firebase collection with its details
       const docRef = await addDoc(collection(FIREBASE_DB, "Task (" + route.params.email + ")"), {
         title,
         note,
@@ -156,25 +174,31 @@ const TaskDetailScreen = ({ navigation, route }) => {
         expectedTime,
         unit,
       })
+
+      // Update the current task ID with the newly created document ID
       route.params.currentTaskID = docRef.id;
 
+      // If there is a parent task, update its subtasks array with the newly created task ID
       if (parentTask) {
-        // Get the parent task object
         const parentTaskObj = tasks.find(task => task.title === parentTask);
         if (parentTaskObj) {
           const parentTaskId = parentTaskObj.id;
           await updateDoc(doc(FIREBASE_DB, "Task (" + route.params.email + ")", parentTaskId), {
-            subtasks: [...parentTaskObj.subtasks, docRef.id], // Assuming subtasks is an array field in the database
+            subtasks: [...parentTaskObj.subtasks, docRef.id], 
           });
         }
       }
       
+      // Log the activity of adding a new task
       const docRef2 = await addDoc(collection(FIREBASE_DB, "Activity (" + route.params.email + ")"), {
         Action: "ADD",
         TaskID: docRef.id,
         Time: serverTimestamp()
-      });  
+      }); 
+
+    // If an existing task is being edited
     }else{
+      // Update an existing task document in the Firebase collection with its revised details
       const docRef = await updateDoc(doc(FIREBASE_DB, "Task (" + route.params.email + ")", route.params.currentTaskID), {
         title,
         note,
@@ -190,6 +214,7 @@ const TaskDetailScreen = ({ navigation, route }) => {
         unit,
       })
 
+      // Log the activity of editing an existing task
       const docRef2 = await addDoc(collection(FIREBASE_DB, "Activity (" + route.params.email + ")"), {
         Action: "EDIT",
         TaskID: route.params.currentTaskID,
@@ -197,22 +222,32 @@ const TaskDetailScreen = ({ navigation, route }) => {
       });  
     }
 
+    // Set the notification to be scheduled
     setNotificationScheduled(true);
 
+    // Navigate back to the Home screen
     navigation.navigate('Home', route.params);
   };
 
   return (
+    // Dismiss keyboard when tapping outside of input field
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      {/* Container for the details of the task */}
       <View style={styles.container}>
+        {/* Render the appropriate title based on whether it's the creation or editing of the task */}
         <Text style={styles.title}>{route.params.currentTaskID == null ? "Create New Task" : "Edit Existing Task"}</Text>
+        {/* Hint for the title field */}
         <Text style={styles.hintText}>Hint: Title field is required</Text>
+        
+        {/* Input field for the title of the task */}
         <TextInput
           style={[styles.input,{fontSize: 20}]}
           placeholder="Title"
           value={title}
           onChangeText={setTitle}
         />
+        
+        {/* Multiline input field for the note of the task */}
         <TextInput
           style={[styles.input, styles.multiline,{fontSize: 16}]}
           placeholder="Note"
@@ -220,7 +255,10 @@ const TaskDetailScreen = ({ navigation, route }) => {
           value={note}
           onChangeText={setNote}
         />
+
+        {/* Container for selecting the parent task */}
         <View style={styles.parentTaskContainer}>
+          {/* Dropdown menu for selecting the parent task */}
           <ModalDropdown
             options={existingTasks.map(existingTasks => "id(" + existingTasks + "): " + tasks.find(obj => obj.id === existingTasks).title)}
             onSelect={handleParentTask}
@@ -231,40 +269,43 @@ const TaskDetailScreen = ({ navigation, route }) => {
             defaultValue="Select Parent Task"
           />
         </View>
+
+        {/* Container for the tags of the task */}
         <View style={styles.tagContainer}>
           <View style={[{flexDirection: "col", width:"15%"}]}>
             <Text style={styles.label}>Tag:</Text>
+            {/* Modal for adding tags */}
             <AddTagsModal 
               onTagSelect={handleTagSelect} 
               tags={route.params && route.params.tags ? route.params.tags : []} 
             />
           </View>
-
           <View style={styles.tagInputs}>
+            {/* Render the selected tags */}
             <ScrollView contentContainerStyle={[{flexDirection: 'row', flexWrap: 'wrap'}]}>
               {selectedTags.map((tag, index) => (
+                // Touchable area to toggle the selection state of a tag 
                 <TouchableOpacity
                   key={index}
                   onPress={() => toggleTagSelection(index)}
-                  style={[
-                    styles.tag,
-                    {
-                      backgroundColor: tag.selected ? '#add8e6' : '#e6e6e6',
-                    },
-                  ]}
+                  style={[styles.tag, {backgroundColor: tag.selected ? '#add8e6' : '#e6e6e6'}]}
                   multiline
                 >
                   <Text style={styles.tagText}>{tag.text}</Text>
+                  {/* Button to remove selected tags */}
                   <TouchableOpacity onPress={() => removeTag(index)}>
                     <FontAwesome name="times" size={15} color="red" />
                   </TouchableOpacity>
                 </TouchableOpacity>
               ))}
-          </ScrollView>
+            </ScrollView>
           </View>
-          </View>
+        </View>
+
+        {/* Container for the start date of the task */}
         <View style={styles.dateInputContainer}>
           <Text style={styles.label}>Start Date:</Text>
+          {/* Input fields for start date of the task */}
           <View style={styles.dateInputs}>
             <TextInput
               style={[styles.dateInput, styles.dayInput]}
@@ -294,8 +335,11 @@ const TaskDetailScreen = ({ navigation, route }) => {
             />
           </View>
         </View>
+
+        {/* Container for due date of the task */}
         <View style={styles.dateInputContainer}>
           <Text style={styles.label}>Due Date:</Text>
+          {/* Input fields for due date of the task */}
           <View style={styles.dateInputs}>
             <TextInput
               style={[styles.dateInput, styles.dayInput]}
@@ -325,7 +369,10 @@ const TaskDetailScreen = ({ navigation, route }) => {
             />
           </View>
         </View>
+
+        {/* Container for expected completion time and notification of the task */}
         <View style={styles.reminderContainer}>
+          {/* Input field for expected completion time of the task */}
           <TextInput
             style={styles.expectedTime}
             placeholder="Expected Time to Complete"
@@ -333,6 +380,7 @@ const TaskDetailScreen = ({ navigation, route }) => {
             value={expectedTime}
             onChangeText={text => setExpectedTime(text.replace(/[^0-9]/g, ''))}
           />
+          {/* Dropdown menu for selecting the unit of expected completion time */}
           <ModalDropdown
             style={styles.units}
             options={['seconds', 'minutes', 'hours', 'days', 'months', 'years']}
@@ -340,14 +388,19 @@ const TaskDetailScreen = ({ navigation, route }) => {
             defaultIndex={0}
             defaultValue= {unit}
           />
+          {/* Component for setting the task notification */}
           <SetReminderScreen
             title={title}
           />
         </View>
+
+        {/* Container for buttons */}
         <View style={styles.buttonContainer}>
+          {/* Button to cancel task creation or editing */}
           <TouchableOpacity style={styles.button} onPress={handleCancel}>
             <Text style={styles.buttonText}>Cancel</Text>
           </TouchableOpacity>
+          {/* Button to save task creation or editing */}
           <TouchableOpacity style={styles.button} onPress={handleSave}>
             <Text style={styles.buttonText}>Save</Text>
           </TouchableOpacity>
@@ -357,46 +410,51 @@ const TaskDetailScreen = ({ navigation, route }) => {
   );
 };
 
+// Styles for the task detail screen
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'flex-start',
+    backgroundColor: 'white',
     flex: 1,
     paddingHorizontal: 20,
     paddingTop: 20,
-    alignItems: 'flex-start',
-    backgroundColor: 'white',
   },
   title: {
+    color: '#379EE8',
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 50,
-    color: '#379EE8',
     left: 90,
-},
+    marginBottom: 50,
+  },
+  hintText: {
+    color: '#888',
+    fontSize: 12,
+  },  
   input: {
-    borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
-    padding: 10,
+    borderWidth: 1,
     marginBottom: 10,
+    padding: 10,
     width: '100%',
   },
   multiline: {
     height: 100,
     textAlignVertical: 'top',
   },
-  button: {
-    backgroundColor: '#007BFF',
-    padding: 15,
-    borderRadius: 5,
-    alignItems: 'center',
-    marginTop: 10,
-    width: '40%',
-  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '100%',
     marginTop: 30,
+    width: '100%',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#007BFF', 
+    borderRadius: 5,
+    marginTop: 10,
+    padding: 15,
+    width: '40%',
   },
   buttonText: {
     color: '#FFF',
@@ -405,76 +463,76 @@ const styles = StyleSheet.create({
   parentTaskContainer: {
     borderColor: '#ccc',
     justifyContent: 'center',
-    width: '100%',
     marginBottom: 10,
+    width: '100%',
   },
   dropdownText: {
-    fontSize: 16,
-    padding: 10,
-    borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
+    borderWidth: 1,
     color: '#333',
+    fontSize: 16,
+    padding: 10,
     width: '100%',
   },
   dropdownItemText: {
     fontSize: 16,
   },
   dropdown: {
-    width: '90%',
-    borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
+    borderWidth: 1,
+    width: '90%',
   },
   tagContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
-    marginBottom: 10,
-  },
-  tagInputs: {
-    alignItems: 'left',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-    height: 90,
-    marginLeft: "5%",
-    width: "80%",
-  },
-  tag: {
     flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 10,
     marginBottom: 10,
-    padding: 5,
-    borderRadius: 5,
-    borderColor: '#ccc',
-    backgroundColor: '#e6e6e6',
-  },
-  dateInputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 10,
-    alignItems: 'flex-start',
-    width: '100%',
   },
   label: {
     fontSize: 20,
     marginRight: 10,
     textAlign: 'left',
   },
-  dateInputs: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  dateInput: {
-    borderWidth: 1,
+  tagInputs: {
+    alignItems: 'left',
     borderColor: '#ccc',
     borderRadius: 5,
-    paddingHorizontal: 10,
-    width: 60,
+    borderWidth: 1,
+    height: 90,
+    marginLeft: "5%",
+    padding: 10,
+    width: "80%",
+  },
+  tag: {
+    alignItems: 'center',
+    backgroundColor: '#e6e6e6',
+    borderColor: '#ccc',
+    borderRadius: 5,
+    flexDirection: 'row',
+    marginBottom: 10,
+    marginRight: 10,
+    padding: 5,
+  },
+  dateInputContainer: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10, 
+    width: '100%',
+  },
+  dateInputs: {
+    alignItems: 'center',
+    flexDirection: 'row',  
+  },
+  dateInput: {
+    borderColor: '#ccc',
+    borderRadius: 5,
+    borderWidth: 1,
     height: 30,
     marginLeft: 10,
+    paddingHorizontal: 10,
+    width: 60, 
   },
   dayInput: {
     marginRight: '2%',
@@ -486,28 +544,24 @@ const styles = StyleSheet.create({
     marginRight: 0,
   },
   reminderContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
+    flexDirection: 'row',
   },
   expectedTime: {
-    borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 5,
+    borderWidth: 1,
     padding: 10,
     width: 210,
   },
   units: {
-    width: 80,
-    height: 40,
-    borderWidth: 1,
     borderRadius: 5,
-    padding: 10,
+    borderWidth: 1,
+    height: 40,
     marginLeft: 10,
     marginRight: 10,
-  },
-  hintText: {
-    fontSize: 12,
-    color: '#888',
+    padding: 10,
+    width: 80,
   },
 });
 
