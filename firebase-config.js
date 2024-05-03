46bf798fee6fd1f66a2cd7dab9dcb17e4d8f4862
Yitong-C <112016972+Yitong-C@users.noreply.github.com>
2024-05-03 06:10:54 -0400

    Add comment to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,6 +3,9 @@ import { initializeAuth, getReactNativePersistence, getAuth } from "firebase/com
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/compat/firestore';
 
+/**
+ * Firebase configuration object containing settings for Firebase services
+ */
 const firebaseConfig = {
     apiKey: "AIzaSyAPJbr9fq7RQRVxi9l52Eb_0OE07ILfThU",
     authDomain: "team-10-f81c2.firebaseapp.com",
@@ -10,15 +13,18 @@ const firebaseConfig = {
     storageBucket: "team-10-f81c2.appspot.com",
     messagingSenderId: "168094770908",
     appId: "1:168094770908:web:8e43509c7560e500bc7619"
-    // measurementId: "G-9QYS5XVZ2K"
-    // databaseURL: 'https://todoapp-XXXXXX.firebaseio.com',
 };
 
+/**
+ * Initialize app base on firebase configuration if didn't
+ */
 if (!firebase.apps.length){
   firebase.initializeApp(firebaseConfig);
 }
+
+/**
+ * Export Firebase app, authentication, firestore instance
+ */
 export const FIREBASE_APP = firebase;
-// export const FIREBASE_APP = initializeApp(firebaseConfig);
-// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIREBASE_AUTH = firebase.auth();
 export const FIREBASE_DB = firebase.firestore();
